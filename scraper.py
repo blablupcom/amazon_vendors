044b69939e0b38411905dad3d49a74a9c83a1308
blablupcom <o.volodin@yahoo.com>
2015-07-29 23:24:13 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -11,21 +11,20 @@ def scrape_asins(base_url, vendors):
         asin_num = []
         for page in itertools.count():
             data = {'seller':'{}'.format(vendor), 'currentPage': '{}'.format(page + 1), 'useMYI': '0'}
-            print data
             pages = requests.post(base_url, data=data)
             soup = json.loads(pages.text)
             if soup:
                 asin_num.extend(soup)
             else:
                 break
-        for i, asin in enumerate(asin_num):
+        for asin in asin_num:
             prices = requests.get('http://www.amazon.com/gp/aag/ajax/asinRenderToJson.html?id={0}&useMYI=0&numCellsInResultsSet=2400&isExplicitSearch=0&merchantID={1}&shovelerName=AAGProductWidget&maxCellsPerPage=1'.format(asin, vendor))
             li = json.loads(prices.text)
             sp = bs(li[0]['content'])
             price = sp.find('li', 'AAG_ProductPrice aagItemDetLI').text
             todays_date = str(datetime.now())
             scraperwiki.sqlite.save(unique_keys=['Date'], data={"Seller ID": vendor, "ASIN": asin.strip(), "Price": price.strip(), "Date": todays_date})
-            print i, price, asin
+            print asin
 
 
 def scrape(vendors):
@@ -34,5 +33,5 @@ def scrape(vendors):
 
 
 if __name__ == '__main__':
-    vendors = ['A1RA39XAINC4DN']
+    vendors = ['A1RA39XAINC4DN', 'A39ZBH3YODSBQD']
     scrape(vendors)
